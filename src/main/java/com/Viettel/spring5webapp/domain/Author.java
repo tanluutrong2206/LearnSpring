d01b887967c676601ada7d3ffa573a6ccdddb77b
Tấn Lưu Trọng <tanluutrong2206@gmail.com>
2020-08-11 18:32:19 +0700

    add author controller

--- src/main/java/com/Viettel/spring5webapp/domain/Author.java
+++ src/main/java/com/Viettel/spring5webapp/domain/Author.java
@@ -30,6 +30,14 @@ public class Author {
         this.lastName = lastName;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
